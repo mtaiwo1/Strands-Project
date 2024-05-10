1091ae87320c8838170388ab6c8889b81e4be6be
Folu Taiwo <143032462+mtaiwo1@users.noreply.github.com>
2024-05-09 22:52:41 -0400

    Delete defs.mk

--- defs.mk
+++
@@ -1,9 +0,0 @@
-#makfile fragment for defining variable .c, .h and .o program files      
-#Author: Folu Taiwo                                                            
-#CPSC1070 - Spring 2024                                                       
-#Project 2 - Bookstore sim   
-
-SOURCES = Node.cpp IntNode.cpp Stack.cpp Customer.cpp CustNode.cpp LinkedList.cpp Queue.cpp BookStore.cpp driver.cpp
-HEADERS = BookStore.h CustNode.h Customer.h IntNode.h LinkedList.h Node.h Queue.h Stack.h
-OBJECTS = BookStore.o CustNode.o Customer.o IntNode.o LinkedList.o Node.o Queue.o Stack.o
-
